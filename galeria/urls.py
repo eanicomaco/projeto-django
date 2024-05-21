0135e365a9b471b28485aed71437645356ee354a
eanicomaco <eanicomaco@gmail.com>
2024-05-21 01:18:46 -0300

    ajustes gerais

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, imagem
+# from galeria.views import imagem --> pode ser feito de forma simplificada, conf. acima
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
 ]
