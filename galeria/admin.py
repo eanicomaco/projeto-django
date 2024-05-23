0b74fa7e6e1d672fa494fd5be0332690209023f3
eanicomaco <eanicomaco@gmail.com>
2024-05-22 23:00:18 -0300

    inclui path de imagense campos adicionais em fotografias

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListarFotografia(admin.ModelAdmin):
+    list_display = ('id','nome','categoria', 'legenda','descricao', 'foto', 'data_fotografia', 'ativo')
+    list_display_links = ('id','nome') # informa qual atributo envia para tela de edição
+    search_fields = ('nome',) # tem que colocar uma vírgula no final
+    list_filter = ('categoria',) # tem que colocar uma vírgula no final
+    list_per_page = 10 # cria um paginador
+    list_editable = ('ativo',) # permite editar o atributo diretamente no grid
+    ordering = ('nome',) #modifica a ordenação do painel admin
+
+admin.site.register(Fotografia,ListarFotografia)
--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
+# INDEX TRAZENDO TODOS OS ITENS NA PÁGINA
+# def index(request):
+#     fotografias = Fotografia.objects.all()
+#     return render(request, 'galeria/index.html',{'cards':fotografias})
+
+# INDEX COM FILTRO | Lista apenas objetos ativos
+# def index(request):
+#     fotografias = Fotografia.objects.filter(ativo=True)
+#     return render(request, 'galeria/index.html',{'cards':fotografias})
+
+# INDEX COM FILTRO  E ORDER_BY
 def index(request):
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by('nome').filter(ativo=True)
     return render(request, 'galeria/index.html',{'cards':fotografias})
 
 def imagem(request, foto_id):
