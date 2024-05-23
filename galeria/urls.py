7f1f97d960241e63a54106abf5a32948ea9387bf
eanicomaco <eanicomaco@gmail.com>
2024-05-23 01:29:55 -0300

    incluir feature de busca

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 # from galeria.views import imagem --> pode ser feito de forma simplificada, conf. acima
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:foto_id>', imagem, name='imagem')
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('buscar/', buscar, name='buscar'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -19,3 +19,12 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html',{'fotografia':fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('nome').filter(ativo=True)
+
+    if "buscar" in request.GET:
+        criterio = request.GET['buscar']
+        if criterio:
+            fotografias = fotografias.filter(nome__icontains=criterio) #icontains é tipo LIKE '%crit%'
+    return render(request, "galeria/buscar.html",{'cards':fotografias})
