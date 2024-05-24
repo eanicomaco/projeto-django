d606f3c115af779f83ccf608d3dc37e20b1ab443
eanicomaco <eanicomaco@gmail.com>
2024-05-24 10:03:27 -0300

    ajustes nos scripts de login e cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,52 +1,61 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib import messages, auth
 
 def login(request):
-    form = request.POST
 
     if request.method == 'POST':
-        login = form.get('login')
-        senha = form.get('senha')
-        user = auth.authenticate(username=login, password=senha)
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        user = authenticate(username=usuario, password=senha)
 
         if user is not None:
-            auth.login(request,user)
-            messages.success(f'Olá {login}! Você logou com sucesso!')
+            login(request,user)
+            messages.success(f'Olá {usuario}! Você logou com sucesso!')
             return redirect('index')
         else:
             messages.error('Falha ao tentar logar no sistema')
             return redirect('login')
 
     else:
-        return render(request, 'usuarios/login.html',{'form':form})
+        return render(request, 'usuarios/login.html')
 
 def cadastro(request):
-    form = request.POST
+
     if request.method == 'POST':
-        usuario = form.get('usuario')
-        email = form.get('email')
-        senha1 = form.get('senha1')
-        senha2 = form.get('senha2')
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha1 = request.POST.get('senha1')
+        senha2 = request.POST.get('senha2')
+
+        data = {
+            'usuario':usuario,
+            'email':email,
+        }
 
         if senha1 != senha2:
             messages.error(request, 'As senhas não conferem')
-            return redirect('cadastro')
+            return redirect('cadastro', {'form':data})
 
         if User.objects.filter(username=usuario).exists():
             messages.error(request, 'Nome de usuário já está em uso')
-            return redirect('cadastro')
+            return redirect('cadastro', {'form':data})
 
         user = User.objects.create_user(username=usuario, email=email, password=senha1)
-        user = authenticate(username=usuario, password=senha1)
 
-        if user is not None:
-            messages.success(request, 'Usuário cadastrado com sucesso')
-            return redirect('login')
+        if user:
+            user = authenticate(username=usuario, password=senha1)
+            if user is not None:
+                messages.success(request, 'Usuário cadastrado com sucesso')
+                return redirect('login')
+            else:
+                messages.error(request, 'Falha ao autenticar o usuário após o cadastro')
+                return redirect('cadastro', {'form':data})
         else:
             messages.error(request, 'Falha ao cadastrar o usuário')
-            return redirect('cadastro')
+            return redirect('cadastro', {'form':data})
 
     else:
+        messages.error(request, 'Operação ilegal')
         return render(request, 'usuarios/cadastro.html')
