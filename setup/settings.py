bd43cbaa9d406040089f568c5c666108420f3ccb
eanicomaco <eanicomaco@gmail.com>
2024-05-25 12:37:51 -0300

    altera nome de media para storage e faz ajustes de referenciamento

--- setup/settings.py
+++ setup/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/storage/'
 
 
 # Default primary key field type
