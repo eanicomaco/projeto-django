1951b2ed39179caaeee722535095819ffb874f89
eanicomaco <eanicomaco@gmail.com>
2024-05-26 00:25:20 -0300

    ajusta rotas e design do buscador

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from apps.galeria.views import index, imagem, buscar
-# from galeria.views import imagem --> pode ser feito de forma simplificada, conf. acima
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('imagem/<int:foto_id>/show', imagem, name='imagem.show'),
     path('buscar/', buscar, name='buscar'),
 ]
